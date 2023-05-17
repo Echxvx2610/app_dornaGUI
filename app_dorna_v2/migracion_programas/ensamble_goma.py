35856101c3c703ba92942e8c04eb60425d892fdc
Cristian Echevarria <99057175+Echxvx2610@users.noreply.github.com>
2023-05-17 01:25:40 -0700

    Update ensamble_goma.py
    trabajando en mmigracion de codigo....

--- app_dorna_v2/migracion_programas/ensamble_goma.py
+++ app_dorna_v2/migracion_programas/ensamble_goma.py
@@ -9,7 +9,7 @@ def p8678(v, a, tq, t1, t2):
     '''
     p8678:
         Es el programa principal del ensamble de goma.haciendo referencia al No.PCB
-        aqui se crea una instancia de la clase Dorna() de la libreria local dorna2
+        Aqui se crea una instancia de la clase Dorna() de la libreria local dorna2
         
         --Parametro de entrada
             v = velocidad
@@ -33,6 +33,7 @@ def p8678(v, a, tq, t1, t2):
                 out5 = Dispensador de goma
                 out2 = Clamp Conveyor
                 out1 = stopper
+                out3 = Linea de SMEMA
                 
     
     '''
@@ -57,7 +58,8 @@ def p8678(v, a, tq, t1, t2):
             
             Para continuar operando la entrada 4, debe tener un valor de 0;
             
-            **nota: la entrada o input hace referencia al boton de emergencia    
+            **nota: la entrada o input hace referencia al boton de emergencia
+             
         '''
         robot.probe(index=4, val=1)
         #robot.wait(in4=1)
@@ -99,8 +101,7 @@ def p8678(v, a, tq, t1, t2):
             #cstp.complete()
         else:
             stp()
-
-            # load()
+            
 
     def unload():
         '''
@@ -119,7 +120,7 @@ def p8678(v, a, tq, t1, t2):
             ustp = robot.play(track=True, cmd="output", out1=1, id=62)  # subimos stopper
             ustp.complete()
         else:
-            print("error el stopper esta arriba")
+            print("Error el stopper esta arriba")
             # unload()
 
     def stp():
@@ -131,28 +132,16 @@ def p8678(v, a, tq, t1, t2):
 
     def stby():
         robot.play(timeout=0-1, cmd="lmove", rel=0, x=459.000, y=6.000, z=298.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=100)
-        #w = robot.play(track=True, cmd="lmove", rel=0, x=459.000, y=6.000, z=298.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=100)
-        #w.complete()
-#    thread = threading.Thread(target=boton_stop)
-#    thread.start()
+        
     while robot.get_input(index=4) == 0:
-        #robot.sys["in4"]
-        # if robot.sys["in3"] == 0:
         load()
-
-        # robot.play(track=True, cmd="lmove", rel=0, x=459.000, y=6.000, z=298.000, a=2.85, b=0, vel=spd, accel=acel, jerk=torq, id=264)
         stby()
         time.sleep(t)
         # puntos de prueba iniciales
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=32.700, z=154.700, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=200)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=32.700, z=152.750, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=201)
-        #p1.complete()
         print(robot.sys)
-        #    print("comm complete")
-        #
         d1 = robot.play(timeout=-1, cmd="output", out5=1, id=202)
-        #d1.complete()
-
         print(d1)
         time.sleep(t)
         robot.play(timeout=-1,cmd="output", out5=0, id=203)
@@ -161,9 +150,7 @@ def p8678(v, a, tq, t1, t2):
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=28.800, z=154.700, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=205)
 #PUNTO 2
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=28.800, z=152.700, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=206)
-        #p2.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=207)
-        #d2.complete()
         time.sleep(t)
         robot.play(timeout=-1,cmd="output", out5=0, id=208)
         print("punto 2")
@@ -171,9 +158,7 @@ def p8678(v, a, tq, t1, t2):
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=24.600, z=154.700, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=210)
  #PUNTO 3
         robot.play(timeout=-1, cmd="lmove", rel=0, x=468.000, y=24.600, z=152.700, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=211)
-        #p3.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=212)
-        #d3.complete()
         time.sleep(t)
         robot.play(timeout=-1,cmd="output", out5=0, id=213)
         print("punto 3")
@@ -183,10 +168,8 @@ def p8678(v, a, tq, t1, t2):
     # 1-L4
         # L4-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=408.394, y=47.117, z=152.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=215)
-        robot.play(timeout=-1, cmd="lmove", rel=0, x=408.394, y=47.117, z=150.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=216)
-        #g1_1.complete(time_out=0)
+        robot.play(timeout=-1, cmd="lmove", rel=0, x=408.394, y=47.117, z=150.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=216))
         robot.play(timeout=-1, cmd="output", out5=1, id=217)
-        #d4_1.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=218)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=408.394, y=47.117, z=152.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=219)
@@ -195,18 +178,13 @@ def p8678(v, a, tq, t1, t2):
         # C25-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.153, y=38.054, z=152.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=225)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.153, y=38.054, z=150.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=226)
-        #g3_1.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=227)
-        #d6_1.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=228)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.153, y=38.054, z=152.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=229)
-        # C25-2
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.22, y=40.983, z=152.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=230)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.22, y=40.983, z=150.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=231)
-        #g4_1.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=232)
-        #d7_1.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=233)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.22, y=40.983, z=152.000, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=234)
@@ -216,9 +194,7 @@ def p8678(v, a, tq, t1, t2):
         # L4-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=408.718, y=-9.391, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=264)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=408.718, y=-9.391, z=149.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=265)
-        #g1_2.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=266)
-        #d4_2.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=267)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=408.718, y=-9.391, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=268)
@@ -227,18 +203,14 @@ def p8678(v, a, tq, t1, t2):
         # C25-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=388.424, y=-19.273, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=274)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=388.424, y=-19.273, z=149.478, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=275)
-        #g3_2.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=276)
-        #d6_2.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=277)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=388.424, y=-19.273, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=278)
         # C25-2
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.629, y=-15.636, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=279)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.629, y=-15.636, z=149.476, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=280)
-        #g4_2.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=281)
-        #d7_2.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=282)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=382.629, y=-15.636, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=283)
@@ -248,9 +220,7 @@ def p8678(v, a, tq, t1, t2):
         # L4-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=407.456, y=-66.422, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=314)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=407.456, y=-66.422, z=149.423, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=315)
-        #g1_3.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=316)
-        #d4_3.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=317)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=407.456, y=-66.422, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=318)
@@ -259,32 +229,22 @@ def p8678(v, a, tq, t1, t2):
         # C25-1
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.472, y=-75.453, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=324)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.472, y=-75.453, z=149.42, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=325)
-        #g3_3.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=326)
-        #d6_3.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=327)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=387.472, y=-75.453, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=328)
         # C25-2
         robot.play(timeout=-1, cmd="lmove", rel=0, x=381.033, y=-71.95, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=329)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=381.033, y=-71.95, z=149.794, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=330)
-        #g4_3.complete(time_out=0)
         robot.play(timeout=-1, cmd="output", out5=1, id=331)
-        #d7_3.complete()
         time.sleep(t)
         robot.play(timeout=-1, cmd="output", out5=0, id=332)
         robot.play(timeout=-1, cmd="lmove", rel=0, x=381.033, y=-71.95, z=151.500, a=ang, b=0, vel=spd, accel=acel, jerk=torq, id=333)
 
         stby()
-        # robot.play(track=True,cmd="lmove", rel=0, x=459.000, y=20.000, z=298.000, a=-0, b=0, vel=spd, accel=acel, jerk=torq, id=264)
         time.sleep(t)
         time.sleep(1)
-
         unload()
-
-        #    robot.play(cmd="alarm",alarm=1, queue=1)
-        # robot.close()
-
 
 if __name__ == '__main__':
     robot = Dorna()
